c02f4ff091e37adbd6dc3e7e6575b3f5d225e8de
Tomasz Klim <github@tomaszklim.pl>
2018-08-03 22:53:59 +0200

    account testing no longer needs GetUser IAM permission

--- utils/test-account.sh
+++ utils/test-account.sh
@@ -4,15 +4,15 @@
 # So this script can be used straight from cron.
 
 if [ "$1" = "" ]; then
-	echo "usage: $0 <cloud-account> [--json]"
+	echo "usage: $0 <cloud-account> [--arn]"
 	exit 1
 fi
 
 account=$1
-result=`aws --profile $account iam get-user 2>/dev/null`
+result=`aws --profile $account iam get-user 2>&1 |egrep -o 'arn:aws:iam::[0-9]+:user\/[a-zA-Z0-9._-]+'`
 
 if [ "$result" = "" ]; then
 	echo "error: cloud account \"$1\" has invalid credentials"
-elif [ "$2" = "--json" ]; then
+elif [ "$2" = "--arn" ]; then
 	echo "$result"
 fi
