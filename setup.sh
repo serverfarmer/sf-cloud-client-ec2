39736551e4e3106a59f4a9ec55fd2b660fa57f87
Tomasz Klim <github@tomaszklim.pl>
2018-08-11 15:46:55 +0200

    moved awscli setup to separate script, random cron time

--- setup.sh
+++ setup.sh
@@ -1,20 +1,6 @@
-#!/bin/sh
-. /opt/farm/scripts/init
+#!/bin/bash
 
-# http://docs.aws.amazon.com/cli/latest/userguide/installing.html
-
-if grep -qFx $OSVER /opt/farm/ext/cloud-client-ec2/config/awscli-system-repo.conf; then
-	/opt/farm/ext/packages/utils/install.sh awscli
-else
-	/opt/farm/ext/packages/utils/install.sh libyaml-dev libpython-dev python-yaml python-pip
-
-	echo "checking for pip package awscli"
-	if [ "`pip list |grep awscli`" = "" ]; then
-		pip install awscli
-	else
-		pip install --upgrade awscli
-	fi
-fi
+/opt/farm/ext/cloud-client-ec2/setup-awscli.sh
 
 /opt/farm/scripts/setup/extension.sh sf-php
 /opt/farm/scripts/setup/extension.sh sf-versioning
@@ -24,5 +10,5 @@ mkdir -p /etc/local/.cloud/ec2 /var/cache/cloud-config-history
 
 if ! grep -q /opt/farm/ext/cloud-client-ec2/cron /etc/crontab; then
 	echo "setting up crontab entry"
-	echo "45 8 * * * root /opt/farm/ext/cloud-client-ec2/cron/update.sh" >>/etc/crontab
+	echo "$((RANDOM%60)) 8 * * * root /opt/farm/ext/cloud-client-ec2/cron/update.sh" >>/etc/crontab
 fi
