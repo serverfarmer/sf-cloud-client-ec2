82107ea19cdf27d4682f127934ff5fdff65ed2b2
Tomasz Klim <github@tomaszklim.pl>
2022-06-06 09:06:50 +0200

    removed remaining code, updated deprecation notice, archiving repository

--- setup.sh
+++ setup.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-if grep -q /opt/farm/ext/cloud-client-ec2/cron /etc/crontab; then
-	sed -i -e "/\/opt\/farm\/ext\/cloud-client-ec2\/cron/d" /etc/crontab
-fi
-
 echo "skipping setup (deprecated extension)"
